d83b672418ecf74536b7e4b32ef2acfcad04db51
h-kod <husemecen@gmail.com>
2020-12-04 22:42:50 +0300

    Template Complete

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+(function ($) {
+    // Collapse Navbar
+    var navbarCollapse = function () {
+        if ($("#mainNav").offset().top > 1) {
+            $("#mainNav").addClass("navbar-scrolled");
+        } else {
+            $("#mainNav").removeClass("navbar-scrolled");
+        }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+
+    // Magnific popup calls
+    $('#portfolio').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1]
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+        }
+    });
+
+    $("#carouselExampleIndicators").on('slide.bs.carousel', function(event){
+        $(".indicators").css("background-image", $(".carousel-item.active").css("background-image"));
+    });
+
+
+})(jQuery);
+
